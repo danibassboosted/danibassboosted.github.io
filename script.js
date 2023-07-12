a1ce2ae999b240f937b56193b2d238ce78212989
danibassboosted <96196600+danibassboosted@users.noreply.github.com>
2023-07-12 19:10:39 -0300

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const audioPlayer = document.getElementById('audio-player');
     
     // Aquí debes configurar la URL de tu archivo de música
-    const musicURL = 'ruta/al/archivo.mp3';
+    const musicURL = 'Musica/Musica.mp3';
     
     audioPlayer.src = musicURL;
     audioPlayer.play();
