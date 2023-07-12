2e80ba1c4a30b71a235dae3a19b9d2276ef7ff2e
danibassboosted <96196600+danibassboosted@users.noreply.github.com>
2023-07-12 19:50:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const audioPlayer = document.getElementById('audio-player');
     
+    audioPlayer.addEventListener('canplay', function() {
+        audioPlayer.play();
+    });
+    
     audioPlayer.addEventListener('ended', function() {
         audioPlayer.currentTime = 0;
         audioPlayer.play();
